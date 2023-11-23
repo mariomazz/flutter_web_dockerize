e7aae66487f97bc6b1b33b8a0a717684a992dc70
mario <mario.mazzarelli@asfweb.net>
2023-11-23 23:55:15 +0100

    init project

---
+++ README.md
@@ -0,0 +1,24 @@
+# flutter_web_dockerize
+
+😱😱😱😱
+
+commans for docker operations :
+
+    create image :
+
+        - docker build -t flutter_web_dockerize .
+
+    create container and run : 
+
+        - docker run -d -p 8080:80 --name flutter_web_dockerize flutter_web_dockerize
+
+    stop container : 
+
+        - docker stop flutter_web_dockerize
+
+
+resources used for the project :
+
+    - https://docs.docker.com/
+
+    - https://medium.com/@kevinwilliams.dev/building-a-flutter-web-container-3b13f4b2bd0c
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2F99ED)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("DOCKER 🤓"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 250,
+              child: Image.asset("assets/images/docker_logo.png"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
